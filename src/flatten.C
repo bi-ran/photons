bd540275bfd9a2788a5a3eff87fa0444b56eeb7e
Ran Bi <biran@mit.edu>
2019-06-10 15:33:25 -0400

    flatten: update with new toys

--- src/flatten.C
+++ src/flatten.C
@@ -1,12 +1,34 @@
 #include "TFile.h"
 #include "TTree.h"
 #include "TH1.h"
+#include "TCanvas.h"
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "../include/differential_histograms.h"
+#include "../include/integral_angles.h"
+#include "../include/interval.h"
+#include "../include/multival.h"
 
 #include "../include/pjtree.h"
 
 #include "../git/config/include/configurer.h"
 
+#define FP_TH1_FILL (int (TH1::*)(double))&TH1::Fill
+#define FP_TH1_FILLW (int (TH1::*)(double, double))&TH1::Fill
+#define FP_TH1_GETBC (double (TH1::*)(int) const)&TH1::GetBinContent
+#define FP_TH1_SETBC (void (TH1::*)(int, double))&TH1::SetBinContent
+#define FP_TH1_DRAW (void (TH1::*)(char const*))&TH1::Draw
+
+using namespace std::literals::string_literals;
+
+using dhist = differential_histograms;
+
 int flatten(char const* config, char const* output) {
+    printf("load config options..\n");
+
     auto conf = new configurer(config);
 
     auto input = conf->get<std::string>("input");
@@ -20,44 +42,229 @@ int flatten(char const* config, char const* output) {
     auto trk_pt_min = conf->get<float>("trk_pt_min");
     auto trk_eta_abs = conf->get<float>("trk_eta_abs");
 
+    auto rpt = conf->get<std::vector<float>>("pt_range");
+    auto rtrkpt = conf->get<std::vector<float>>("trkpt_range");
+    auto rdphi = conf->get<std::vector<float>>("dphi_range");
+    auto rx = conf->get<std::vector<float>>("x_range");
+    auto rntrk = conf->get<std::vector<float>>("ntrk_range");
+    auto rsumpt = conf->get<std::vector<float>>("sumpt_range");
+
+    auto dpt = conf->get<std::vector<float>>("pt_diff");
+    auto ddphi = conf->get<std::vector<float>>("dphi_diff");
+    auto dntrk = conf->get<std::vector<float>>("ntrk_diff");
+    auto dsumpt = conf->get<std::vector<float>>("sumpt_diff");
+
+    /* convert to integral angle units (cast to double) */
+    for (auto& i : rdphi) { i = convert_degrees(i); }
+    for (auto& i : ddphi) { i = convert_degrees(i); }
+
+    printf("prepare histograms..\n");
+
+    auto incl = std::make_shared<interval>(0.f, 9999.f);
+    auto ipt = std::make_shared<interval>(dpt);
+    auto idphi = std::make_shared<interval>(ddphi);
+    auto intrk = std::make_shared<interval>(dntrk);
+    auto isumpt = std::make_shared<interval>(dsumpt);
+
+    auto mincl = std::make_shared<multival>(*incl);
+    auto mpt = std::make_shared<multival>(dpt);
+    auto mdphi = std::make_shared<multival>(ddphi);
+    auto mptdphi = std::make_shared<multival>(dpt, ddphi);
+    auto mptntrk3sumpt3 = std::make_shared<multival>(
+        dpt, dntrk, dntrk, dntrk, dsumpt, dsumpt, dsumpt);
+
+    auto photon_f_pt = std::make_unique<dhist>("photon_f_pt"s, rpt, mincl);
+
+    auto ntrk = std::make_unique<dhist>("ntrk"s, incl, mdphi);
+    auto sumpt = std::make_unique<dhist>("sumpt"s, incl, mdphi);
+
+    auto evt_f_ntrk = std::make_unique<dhist>(
+        "evt_f_ntrk"s, rntrk, mptdphi);
+    auto evt_f_sumpt = std::make_unique<dhist>(
+        "evt_f_sumpt"s, rsumpt, mptdphi);
+
+    auto trk_f_dphi = std::make_unique<dhist>("trk_f_dphi"s, rdphi, mpt);
+    auto trk_f_pt = std::make_unique<dhist>("trk_f_pt"s, rtrkpt, mptdphi);
+
+    auto pjet_f_dphi = std::make_unique<dhist>(
+        "pjet_f_dphi"s, rdphi, mptntrk3sumpt3);
+    auto pjet_f_jetpt = std::make_unique<dhist>(
+        "pjet_f_jetpt"s, rpt, mptntrk3sumpt3);
+    auto pjet_f_x = std::make_unique<dhist>(
+        "pjet_f_x"s, rx, mptntrk3sumpt3);
+
+    printf("iterate..\n");
+
     TFile* f = new TFile(input.data(), "read");
-    TTree* t = (TTree*)f->Get("pyjamas");
-    auto pjt = new pjtree(t, mc_branches);
+    TTree* t = (TTree*)f->Get("pj");
+    auto pjt = new pjtree(mc_branches, t);
 
     TFile* fout = new TFile(output, "recreate");
 
-    TH1F* h_photon_pt = new TH1F("h_photon_pt", "", 100, 0, 200);
-    TH1F* h_jet_pt = new TH1F("h_jet_pt", "", 100, 0, 200);
-    TH1F* h_trk_pt = new TH1F("h_trk_pt", "", 100, 0, 200);
+    /* manage memory manually */
+    TH1::AddDirectory(false);
+    TH1::SetDefaultSumw2();
 
-    int64_t nentries = std::min((int64_t)t->GetEntries(), max_entries);
+    int64_t nentries = static_cast<int64_t>(t->GetEntries());
+    if (max_entries) { nentries = std::min(nentries, max_entries); }
     for (int64_t i = 0; i < nentries; ++i) {
+        if (i % 1000 == 0) { printf("entry: %li/%li\n", i, nentries); }
+
         t->GetEntry(i);
 
-        for (int j = 0; j < pjt->nPho; ++j) {
-            if ((*pjt->phoEt)[j] > photon_pt_min
-                    && std::abs((*pjt->phoEta)[j]) < photon_eta_abs) {
-                h_photon_pt->Fill((*pjt->phoEt)[j]);
-            }
+        /* event selections */
+
+        int64_t photon_leading = -1;
+        for (int64_t j = 0; j < pjt->nPho; ++j) {
+            if ((*pjt->phoEt)[j] < photon_pt_min) { continue; }
+            if (std::abs((*pjt->phoEta)[j]) > photon_eta_abs) { continue; }
+            if ((*pjt->phoSigmaIEtaIEta_2012)[j] > 0.01) { continue; }
+
+            /* isolation requirement */
+
+            photon_leading = j;
+            break;
         }
 
-        for (int j = 0; j < pjt->nref; ++j) {
-            if ((*pjt->jtpt)[j] > jet_pt_min
-                    && std::abs((*pjt->jteta)[j]) < jet_eta_abs) {
-                h_jet_pt->Fill((*pjt->jtpt)[j]);
-            }
+        /* require leading photon */
+        if (photon_leading < 0) { continue; }
+
+        double photon_leading_pt = (*pjt->phoEt)[photon_leading];
+        int64_t photon_pt_x = ipt->index_for(photon_leading_pt);
+
+        (*photon_f_pt)(0, FP_TH1_FILL, photon_leading_pt);
+
+        /* set (phi) axis of leading photon */
+        auto photon_phi = convert_radians((*pjt->phoPhi)[photon_leading]);
+
+        /* clear counts (sums) */
+        for (int64_t j = 0; j < mdphi->size(); ++j) {
+            (*ntrk)(j, FP_TH1_SETBC, 1, 0.);
+            (*sumpt)(j, FP_TH1_SETBC, 1, 0.);
         }
 
-        for (int j = 0; j < pjt->nTrk; ++j) {
-            if ((*pjt->trkPt)[j] > trk_pt_min
-                    && std::abs((*pjt->trkEta)[j]) < trk_eta_abs) {
-                h_trk_pt->Fill((*pjt->trkPt)[j]);
-            }
+        for (int64_t j = 0; j < pjt->nTrk; ++j) {
+            if ((*pjt->trkPt)[j] < trk_pt_min) { continue; }
+            if (std::abs((*pjt->trkEta)[j]) > trk_eta_abs) { continue; }
+
+            /* track quality selections */
+
+            double trk_pt = (*pjt->trkPt)[j];
+            auto trk_phi = convert_radians((*pjt->trkPhi)[j]);
+            auto photon_trk_dphi = std::abs(photon_phi - trk_phi);
+            int64_t dphi_x = idphi->index_for(photon_trk_dphi);
+
+            (*ntrk)(dphi_x, FP_TH1_FILLW, 1., 1.);
+            (*sumpt)(dphi_x, FP_TH1_FILLW, 1., trk_pt);
+
+            (*trk_f_dphi)(photon_pt_x, FP_TH1_FILL,
+                          static_cast<double>(photon_trk_dphi));
+            (*trk_f_pt)(x{photon_pt_x, dphi_x}, FP_TH1_FILL, trk_pt);
+        }
+
+        for (int64_t j = 0; j < mdphi->size(); ++j) {
+            double evt_ntrk = (*ntrk)(j, FP_TH1_GETBC, 1);
+            double evt_sumpt = (*sumpt)(j, FP_TH1_GETBC, 1);
+
+            (*evt_f_ntrk)(x{photon_pt_x, j}, FP_TH1_FILL, evt_ntrk);
+            (*evt_f_sumpt)(x{photon_pt_x, j}, FP_TH1_FILL, evt_sumpt);
+        }
+
+        double inorm = 2. * trk_eta_abs * 2. * M_PI;
+        double dnorm = inorm / 3.;
+
+        int64_t incl_ntrk_x = intrk->index_for(
+            ntrk->sum(x{0}, 0)->GetBinContent(1) / inorm);
+        int64_t near_ntrk_x = intrk->index_for((*ntrk)[0]->Integral() / dnorm);
+        int64_t perp_ntrk_x = intrk->index_for((*ntrk)[1]->Integral() / dnorm);
+
+        int64_t incl_sumpt_x = isumpt->index_for(
+            sumpt->sum(x{0}, 0)->GetBinContent(1) / inorm);
+        int64_t near_sumpt_x = isumpt->index_for((*sumpt)[0]->Integral() / dnorm);
+        int64_t perp_sumpt_x = isumpt->index_for((*sumpt)[1]->Integral() / dnorm);
+
+#define REPEAT_FOR_ALL_REGIONS(expr)    \
+    incl_##expr, near_##expr, perp_##expr
+
+        for (int64_t j = 0; j < pjt->nref; ++j) {
+            if ((*pjt->jtpt)[j] < jet_pt_min) { continue; }
+            if (std::abs((*pjt->jteta)[j]) > jet_eta_abs) { continue; }
+
+            auto jet_phi = convert_radians((*pjt->jtphi)[j]);
+            auto pjet_dphi = std::abs(photon_phi - jet_phi);
+
+            (*pjet_f_dphi)(x{photon_pt_x,
+                                   REPEAT_FOR_ALL_REGIONS(ntrk_x),
+                                   REPEAT_FOR_ALL_REGIONS(sumpt_x)},
+                                 FP_TH1_FILL,
+                                 static_cast<double>(pjet_dphi));
+
+            /* require back-to-back jets */
+            if (pjet_dphi < 0.875_pi) { continue; }
+
+            double jet_pt = (*pjt->jtpt)[j];
+
+            (*pjet_f_jetpt)(x{photon_pt_x,
+                              REPEAT_FOR_ALL_REGIONS(ntrk_x),
+                              REPEAT_FOR_ALL_REGIONS(sumpt_x)},
+                            FP_TH1_FILL,
+                            jet_pt);
+            (*pjet_f_x)(x{photon_pt_x,
+                          REPEAT_FOR_ALL_REGIONS(ntrk_x),
+                          REPEAT_FOR_ALL_REGIONS(sumpt_x)},
+                        FP_TH1_FILL,
+                        jet_pt / photon_leading_pt);
         }
     }
 
-    fout->Write("", TObject::kOverwrite);
-    fout->Close();
+    /* integrate histograms */
+    /* photon-jet momentum imbalance as function of perp sumpt */
+    auto pjet_f_x_d_near_perp_sumpt = pjet_f_x
+        ->sum(0)    /* photon pt */
+        ->sum(0)    /* incl_ntrk */
+        ->sum(0)    /* near_ntrk */
+        ->sum(0)    /* perp_ntrk */
+        ->sum(0);   /* incl_sumpt */
+
+    auto pjet_f_x_d_perp_sumpt = pjet_f_x_d_near_perp_sumpt
+        ->sum(0);   /* near_sumpt */
+    auto pjet_f_x_d_near_sumpt = pjet_f_x_d_near_perp_sumpt
+        ->sum(1);   /* perp_sumpt */
+
+    printf("painting..\n");
+
+    TCanvas* c1 = new TCanvas("c1", "", 800, 1200);
+
+    c1->Divide(2, 3);
+
+    for (int64_t i = 0; i < isumpt->size(); ++i) {
+        c1->cd(i + 1);
+
+        auto perp_integral = (*pjet_f_x_d_perp_sumpt)[i]->Integral();
+        (*pjet_f_x_d_perp_sumpt)[i]->Scale(1. / perp_integral);
+
+        (*pjet_f_x_d_perp_sumpt)[i]->SetStats(0);
+        (*pjet_f_x_d_perp_sumpt)[i]->SetMarkerStyle(21);
+        (*pjet_f_x_d_perp_sumpt)[i]->SetAxisRange(0, 0.12, "Y");
+        (*pjet_f_x_d_perp_sumpt)(i, FP_TH1_DRAW, "p e");
+
+        auto near_integral = (*pjet_f_x_d_near_sumpt)[i]->Integral();
+        (*pjet_f_x_d_near_sumpt)[i]->Scale(1. / near_integral);
+
+        (*pjet_f_x_d_near_sumpt)[i]->SetStats(0);
+        (*pjet_f_x_d_near_sumpt)[i]->SetLineColor(2);
+        (*pjet_f_x_d_near_sumpt)[i]->SetMarkerColor(2);
+        (*pjet_f_x_d_near_sumpt)[i]->SetMarkerStyle(20);
+        (*pjet_f_x_d_near_sumpt)(i, FP_TH1_DRAW, "same p e");
+    }
+
+    c1->SaveAs("c1.pdf");
+
+    /* fout->Write("", TObject::kOverwrite); */
+
+    (void)fout;
+
+    printf("destroying objects..\n");
 
     return 0;
 }
