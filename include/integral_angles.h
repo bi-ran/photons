c8baea9a9390292bcbf9b4c393877062f5bb2db5
Ran Bi <biran@mit.edu>
2019-06-19 19:27:10 -0400

    integral_angles: standardise conversions
    add reversion function.

--- include/integral_angles.h
+++ include/integral_angles.h
@@ -1,24 +1,36 @@
 #ifndef INTEGRAL_ANGLES_H
 #define INTEGRAL_ANGLES_H
 
-int64_t convert_radians(long double angle) {
-    return (int64_t)(angle * std::numeric_limits<int64_t>::max() / M_PI);
+#include <limits>
+
+constexpr int64_t convert_pis(long double angle) {
+    return static_cast<int64_t>(angle * std::numeric_limits<int64_t>::max());
 }
 
-int64_t convert_degrees(long double angle) {
-    return (int64_t)(angle * std::numeric_limits<int64_t>::max() / 180.);
+constexpr int64_t convert_radians(long double angle) {
+    return static_cast<int64_t>((angle / M_PI)
+        * std::numeric_limits<int64_t>::max());
+}
+
+constexpr int64_t convert_degrees(long double angle) {
+    return static_cast<int64_t>((angle / 180.)
+        * std::numeric_limits<int64_t>::max());
 }
 
 constexpr int64_t operator ""_pi(long double angle) {
-    return (int64_t)(angle * std::numeric_limits<int64_t>::max());
+    return convert_pis(angle);
 }
 
 constexpr int64_t operator ""_rad(long double angle) {
-    return (int64_t)(angle * std::numeric_limits<int64_t>::max() / M_PI);
+    return convert_radians(angle);
 }
 
 constexpr int64_t operator ""_deg(long double angle) {
-    return (int64_t)(angle * std::numeric_limits<int64_t>::max() / 180.);
+    return convert_degrees(angle);
+}
+
+double revert_radians(int64_t angle) {
+    return static_cast<double>(angle) / std::numeric_limits<int64_t>::max();
 }
 
 #endif /* INTEGRAL_ANGLES_H */
