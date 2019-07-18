0871fb9a208a26068113da908d7165b404a1c873
Ran Bi <biran@mit.edu>
2019-07-18 16:25:27 -0400

    diffaxis: update differentiality
    rename some variables for readability, more reasonable differential
    histograms and results. fix normalisation of hf-differential results.

--- src/diffaxis.C
+++ src/diffaxis.C
@@ -32,27 +32,19 @@ using namespace std::literals::string_literals;
 using namespace std::placeholders;
 
 void fill_axes(pjtree* pjt, float jet_pt_min, float jet_eta_abs,
-               double photon_leading_pt, int64_t photon_phi,
-               int64_t photon_pt_x,
-               std::shared_ptr<multival>& mdiff,
+               double photon_pt, int64_t photon_phi,
+               int64_t pt_x, int64_t hf_x, int64_t pthf_x,
+               std::shared_ptr<interval>& ix,
                std::unique_ptr<history>& nevt,
-               std::unique_ptr<history>& pjet_f_x,
                std::unique_ptr<history>& pjet_es_f_dphi,
                std::unique_ptr<history>& pjet_wta_f_dphi,
+               std::unique_ptr<history>& pjet_f_x,
                std::unique_ptr<history>& pjet_f_ddr) {
-    (*nevt)[photon_pt_x]->Fill(1.);
+    (*nevt)[pthf_x]->Fill(1.);
 
     for (int64_t j = 0; j < pjt->nref; ++j) {
         if ((*pjt->jtpt)[j] < jet_pt_min) { continue; }
         if (std::abs((*pjt->jteta)[j]) > jet_eta_abs) { continue; }
-
-        double jet_pt = (*pjt->jtpt)[j];
-        double pjet_x = jet_pt / photon_leading_pt;
-
-        double hf = pjt->hiHF;
-
-        /* calculate index */
-        int64_t index = mdiff->index_for(v{photon_leading_pt, hf, pjet_x});
 
         auto jet_eta = (*pjt->jteta)[j];
         auto jet_phi = convert_radian((*pjt->jtphi)[j]);
@@ -63,19 +55,25 @@ void fill_axes(pjtree* pjt, float jet_pt_min, float jet_eta_abs,
         auto photon_jet_dphi = std::abs(photon_phi - jet_phi);
         auto photon_wta_dphi = std::abs(photon_phi - jet_wta_phi); 
 
-        (*pjet_es_f_dphi)[index]->Fill(photon_jet_dphi);
-        (*pjet_wta_f_dphi)[index]->Fill(photon_wta_dphi);
+        (*pjet_es_f_dphi)[pthf_x]->Fill(photon_jet_dphi);
+        (*pjet_wta_f_dphi)[pthf_x]->Fill(photon_wta_dphi);
 
         /* require back-to-back jets */
         if (photon_jet_dphi < 0.875_pi) { continue; }
 
-        (*pjet_f_x)[photon_pt_x]->Fill(pjet_x);
+        double jet_pt = (*pjt->jtpt)[j];
+        double pjet_x = jet_pt / photon_pt;
+
+        (*pjet_f_x)[pthf_x]->Fill(pjet_x);
+
+        /* calculate index */
+        auto x_x = ix->index_for(pjet_x);
 
         double jt_deta = jet_eta - jet_wta_eta;
         double jt_dphi = revert_radian(jet_phi - jet_wta_phi);
         double jt_dr = jt_deta * jt_deta + jt_dphi * jt_dphi;
 
-        (*pjet_f_ddr)[index]->Fill(std::sqrt(jt_dr));
+        (*pjet_f_ddr)[x{pt_x, hf_x, x_x}]->Fill(std::sqrt(jt_dr));
     }
 }
 
@@ -110,14 +108,14 @@ int diffaxis(char const* config, char const* output) {
     auto input = conf->get<std::string>("input");
     auto mix = conf->get<std::string>("mix");
     auto max_entries = conf->get<int64_t>("max_entries");
-    auto mc_branches = conf->get<bool>("mc_branches");
+    auto frequency = conf->get<int64_t>("frequency");
 
-    auto freq = conf->get<int64_t>("frequency");
+    auto type = conf->get<std::string>("type");
 
-    auto photon_pt_min = conf->get<float>("photon_pt_min");
-    auto photon_eta_abs = conf->get<float>("photon_eta_abs");
-    auto jet_pt_min = conf->get<float>("jet_pt_min");
-    auto jet_eta_abs = conf->get<float>("jet_eta_abs");
+    auto const photon_pt_min = conf->get<float>("photon_pt_min");
+    auto const photon_eta_abs = conf->get<float>("photon_eta_abs");
+    auto const jet_pt_min = conf->get<float>("jet_pt_min");
+    auto const jet_eta_abs = conf->get<float>("jet_eta_abs");
     auto const hovere_max = conf->get<float>("hovere_max");
     auto const see_min = conf->get<float>("see_min");
     auto const see_max = conf->get<float>("see_max");
@@ -129,66 +127,64 @@ int diffaxis(char const* config, char const* output) {
     auto rx = conf->get<std::vector<float>>("x_range");
     auto rdr = conf->get<std::vector<float>>("dr_range");
 
-    auto dppt = conf->get<std::vector<float>>("ppt_diff");
+    auto dpt = conf->get<std::vector<float>>("pt_diff");
     auto dhf = conf->get<std::vector<float>>("hf_diff");
     auto dx = conf->get<std::vector<float>>("x_diff");
 
     auto events_to_mix = conf->get<int64_t>("events_to_mix");
 
-    auto type = conf->get<std::string>("type");
-
     /* convert to integral angle units (cast to double) */
     convert_in_place_pi(rdphi);
 
     /* default values for config options */
-    freq = freq ? freq : 10000;
+    frequency = frequency ? frequency : 10000;
     events_to_mix = std::max(100L, events_to_mix);
 
     printf("prepare histograms..\n");
 
     auto incl = std::make_shared<interval>(1, 0.f, 9999.f);
-    auto ippt = std::make_shared<interval>(dppt);
+    auto ipt = std::make_shared<interval>(dpt);
     auto ihf = std::make_shared<interval>(dhf);
     auto ix = std::make_shared<interval>(dx);
 
     auto mincl = std::make_shared<multival>(*incl);
-    auto mppt = std::make_shared<multival>(dppt);
-    auto mdiff = std::make_shared<multival>(dppt, dhf, dx);
+    auto mpthf = std::make_shared<multival>(dpt, dhf);
+    auto mpthfx = std::make_shared<multival>(dpt, dhf, dx);
 
-    auto nevt = std::make_unique<history>("nevt"s, "", incl, mppt);
-    auto nmix = std::make_unique<history>("nmix"s, "", incl, mppt);
+    auto nevt = std::make_unique<history>("nevt"s, "", incl, mpthf);
+    auto nmix = std::make_unique<history>("nmix"s, "", incl, mpthf);
 
     auto photon_f_pt = std::make_unique<history>("photon_f_pt"s,
         "dN/dp_{T}^{#gamma}", "p_{T}^{#gamma}", rppt, mincl);
 
-    auto pjet_f_x = std::make_unique<history>("pjet_f_x"s,
-        "dN/dx^{#gammaj}", "x^{#gammaj}", rx, mppt);
-    auto mix_pjet_f_x = std::make_unique<history>("mix_pjet_f_x"s,
-        "dN/dx^{#gammaj}", "x^{#gammaj}", rx, mppt);
-
     auto pjet_es_f_dphi = std::make_unique<history>("pjet_es_f_dphi"s,
-        "dN/d#Delta#phi^{#gammaj}", "#Delta#phi^{#gammaj}", rdphi, mdiff);
+        "dN/d#Delta#phi^{#gammaj}", "#Delta#phi^{#gammaj}", rdphi, mpthf);
     auto pjet_wta_f_dphi = std::make_unique<history>("pjet_wta_f_dphi"s,
-        "dN/d#Delta#phi^{#gammaj}", "#Delta#phi^{#gammaj}", rdphi, mdiff);
-    auto pjet_f_ddr = std::make_unique<history>("pjet_f_ddr"s,
-        "dN/d#Deltar^{jj}", "#Deltar^{jj}", rdr, mdiff);
+        "dN/d#Delta#phi^{#gammaj}", "#Delta#phi^{#gammaj}", rdphi, mpthf);
+    auto pjet_f_x = std::make_unique<history>("pjet_f_x"s,
+        "dN/dx^{#gammaj}", "x^{#gammaj}", rx, mpthf);
 
     auto mix_pjet_es_f_dphi = std::make_unique<history>("mix_pjet_es_f_dphi"s,
-        "dN/d#Delta#phi^{#gammaj}", "#Delta#phi^{#gammaj}", rdphi, mdiff);
+        "dN/d#Delta#phi^{#gammaj}", "#Delta#phi^{#gammaj}", rdphi, mpthf);
     auto mix_pjet_wta_f_dphi = std::make_unique<history>("mix_pjet_wta_f_dphi"s,
-        "dN/d#Delta#phi^{#gammaj}", "#Delta#phi^{#gammaj}", rdphi, mdiff);
+        "dN/d#Delta#phi^{#gammaj}", "#Delta#phi^{#gammaj}", rdphi, mpthf);
+    auto mix_pjet_f_x = std::make_unique<history>("mix_pjet_f_x"s,
+        "dN/dx^{#gammaj}", "x^{#gammaj}", rx, mpthf);
+
+    auto pjet_f_ddr = std::make_unique<history>("pjet_f_ddr"s,
+        "dN/d#Deltar^{jj}", "#Deltar^{jj}", rdr, mpthfx);
     auto mix_pjet_f_ddr = std::make_unique<history>("mix_pjet_f_ddr",
-        "dN/d#Deltar^{jj}", "#Deltar^{jj}", rdr, mdiff);
+        "dN/d#Deltar^{jj}", "#Deltar^{jj}", rdr, mpthfx);
 
     printf("iterate..\n");
 
     TFile* f = new TFile(input.data(), "read");
     TTree* t = (TTree*)f->Get("pj");
-    auto pjt = new pjtree(mc_branches, t);
+    auto pjt = new pjtree(false, t);
 
     TFile* fm = new TFile(mix.data(), "read");
     TTree* tm = (TTree*)fm->Get("pj");
-    auto pjtm = new pjtree(mc_branches, tm);
+    auto pjtm = new pjtree(false, tm);
 
     TFile* fout = new TFile(output, "recreate");
 
@@ -200,111 +196,129 @@ int diffaxis(char const* config, char const* output) {
     if (max_entries) { nentries = std::min(nentries, max_entries); }
     int64_t mentries = static_cast<int64_t>(tm->GetEntries());
     for (int64_t i = 0, m = 0; i < nentries; ++i) {
-        if (i % freq == 0) { printf("entry: %li/%li\n", i, nentries); }
+        if (i % frequency == 0) { printf("entry: %li/%li\n", i, nentries); }
 
         t->GetEntry(i);
 
         /* event selections */
 
-        int64_t photon_leading = -1;
+        int64_t leading = -1;
         for (int64_t j = 0; j < pjt->nPho; ++j) {
             if ((*pjt->phoEt)[j] < photon_pt_min) { continue; }
             if (std::abs((*pjt->phoEta)[j]) > photon_eta_abs) { continue; }
             if ((*pjt->phoHoverE)[j] > hovere_max) { continue; }
 
-            photon_leading = j;
+            leading = j;
             break;
         }
 
-        if ((*pjt->phoSigmaIEtaIEta_2012)[photon_leading] > see_max
-                || (*pjt->phoSigmaIEtaIEta_2012)[photon_leading] < see_min)
+        if ((*pjt->phoSigmaIEtaIEta_2012)[leading] > see_max
+                || (*pjt->phoSigmaIEtaIEta_2012)[leading] < see_min)
             continue;
 
         /* require leading photon */
-        if (photon_leading < 0) { continue; }
+        if (leading < 0) { continue; }
 
         /* isolation requirement */
-        float isolation = (*pjt->pho_ecalClusterIsoR4)[photon_leading]
-            + (*pjt->pho_hcalRechitIsoR4)[photon_leading]
-            + (*pjt->pho_trackIsoR4PtCut20)[photon_leading];
+        float isolation = (*pjt->pho_ecalClusterIsoR4)[leading]
+            + (*pjt->pho_hcalRechitIsoR4)[leading]
+            + (*pjt->pho_trackIsoR4PtCut20)[leading];
         if (isolation > iso_max) { continue; }
 
-        double photon_leading_pt = (*pjt->phoEt)[photon_leading];
-        int64_t photon_pt_x = ippt->index_for(photon_leading_pt);
+        double photon_pt = (*pjt->phoEt)[leading];
+        auto pt_x = ipt->index_for(photon_pt);
 
-        (*photon_f_pt)(0, FP_TH1_FILL, photon_leading_pt);
+        (*photon_f_pt)(0, FP_TH1_FILL, photon_pt);
 
         /* set (phi) axis of leading photon */
-        auto photon_phi = convert_radian((*pjt->phoPhi)[photon_leading]);
+        auto photon_phi = convert_radian((*pjt->phoPhi)[leading]);
+
+        double hf = pjt->hiHF;
+        auto hf_x = ihf->index_for(hf);
+
+        auto pthf_x = mpthf->index_for(x{pt_x, hf_x});
 
         fill_axes(pjt, jet_pt_min, jet_eta_abs,
-                  photon_leading_pt, photon_phi,
-                  photon_pt_x, mdiff, nevt,
-                  pjet_f_x,
-                  pjet_es_f_dphi, pjet_wta_f_dphi,
-                  pjet_f_ddr);
+                  photon_pt, photon_phi,
+                  pt_x, hf_x, pthf_x, ix,
+                  nevt, pjet_es_f_dphi, pjet_wta_f_dphi,
+                  pjet_f_x, pjet_f_ddr);
 
         /* mixing events in minimum bias */
-        for (int64_t k = 0; k < events_to_mix; ++k) {
+        for (int64_t k = 0; k < events_to_mix; m = (m + 1) % mentries) {
             tm->GetEntry(m);
 
             /* hf within +/- 10% */
             if (std::abs(pjtm->hiHF / pjt->hiHF - 1.) > 0.1) { continue; }
 
             fill_axes(pjtm, jet_pt_min, jet_eta_abs,
-                      photon_leading_pt, photon_phi,
-                      photon_pt_x, mdiff, nmix,
-                      mix_pjet_f_x,
-                      mix_pjet_es_f_dphi, mix_pjet_wta_f_dphi,
-                      mix_pjet_f_ddr);
+                      photon_pt, photon_phi,
+                      pt_x, hf_x, pthf_x, ix,
+                      nmix, mix_pjet_es_f_dphi, mix_pjet_wta_f_dphi,
+                      mix_pjet_f_x, mix_pjet_f_ddr);
 
-            m = (m + 1) % mentries;
+            ++k;
         }
     }
 
     /* normalise histograms */
-    scale(1. / events_to_mix, mix_pjet_es_f_dphi, mix_pjet_wta_f_dphi,
+    scale(1. / events_to_mix,
+        mix_pjet_es_f_dphi, mix_pjet_wta_f_dphi,
         mix_pjet_f_ddr, mix_pjet_f_x);
 
     /* integrate histograms */
-    auto pjet_es_f_dphi_d_ppt = pjet_es_f_dphi->sum(1, 1);
-    auto pjet_wta_f_dphi_d_ppt = pjet_wta_f_dphi->sum(1, 1);
-    auto pjet_f_ddr_d_ppt = pjet_f_ddr->sum(1, 1);
-    auto pjet_f_ddr_d_x = pjet_f_ddr->sum(0, 0);
+    auto nevt_d_pt = nevt->sum(1);
+    auto nevt_d_hf = nevt->sum(0);
+
+    auto pjet_es_f_dphi_d_pt = pjet_es_f_dphi->sum(1);
+    auto pjet_wta_f_dphi_d_pt = pjet_wta_f_dphi->sum(1);
+    auto pjet_f_x_d_pt = pjet_f_x->sum(1);
+    auto pjet_f_x_d_hf = pjet_f_x->sum(0);
+
+    auto pjet_f_ddr_d_pt = pjet_f_ddr->sum(1, 1);
+    auto pjet_f_ddr_d_hf = pjet_f_ddr->sum(0, 1);
 
     /* mixed events */
-    auto mix_pjet_es_f_dphi_d_ppt = mix_pjet_es_f_dphi->sum(1, 1);
-    auto mix_pjet_wta_f_dphi_d_ppt = mix_pjet_wta_f_dphi->sum(1, 1);
-    auto mix_pjet_f_ddr_d_ppt = mix_pjet_f_ddr->sum(1, 1);
-    auto mix_pjet_f_ddr_d_x = mix_pjet_f_ddr->sum(0, 0);
+    auto mix_pjet_es_f_dphi_d_pt = mix_pjet_es_f_dphi->sum(1);
+    auto mix_pjet_wta_f_dphi_d_pt = mix_pjet_wta_f_dphi->sum(1);
+    auto mix_pjet_f_x_d_pt = mix_pjet_f_x->sum(1);
+    auto mix_pjet_f_x_d_hf = mix_pjet_f_x->sum(0);
+
+    auto mix_pjet_f_ddr_d_pt = mix_pjet_f_ddr->sum(1, 1);
+    auto mix_pjet_f_ddr_d_hf = mix_pjet_f_ddr->sum(0, 1);
 
     /* subtract histograms */
+    /* *pjet_es_f_dphi -= *mix_pjet_es_f_dphi; */
+    /* *pjet_wta_f_dphi -= *mix_pjet_wta_f_dphi; */
+    /* *pjet_f_ddr -= *mix_pjet_f_ddr; */
     /* *pjet_f_x -= *mix_pjet_f_x; */
 
-    /* *pjet_es_f_dphi_d_ppt -= *mix_pjet_es_f_dphi_d_ppt; */
-    /* *pjet_wta_f_dphi_d_ppt -= *mix_pjet_wta_f_dphi_d_ppt; */
-    /* *pjet_f_ddr_d_ppt -= *mix_pjet_f_ddr_d_ppt; */
-
     /* normalise to number of photons (events) */
-    normalise(nevt,
-        pjet_es_f_dphi_d_ppt, pjet_wta_f_dphi_d_ppt,
-        mix_pjet_es_f_dphi_d_ppt, mix_pjet_wta_f_dphi_d_ppt,
-        pjet_f_ddr_d_ppt, mix_pjet_f_ddr_d_ppt,
-        pjet_f_x, mix_pjet_f_x);
+    normalise(nevt_d_pt,
+        pjet_es_f_dphi_d_pt, mix_pjet_es_f_dphi_d_pt,
+        pjet_wta_f_dphi_d_pt, mix_pjet_wta_f_dphi_d_pt,
+        pjet_f_ddr_d_pt, mix_pjet_f_ddr_d_pt,
+        pjet_f_x_d_pt, mix_pjet_f_x_d_pt);
+
+    normalise(nevt_d_hf,
+        pjet_f_ddr_d_hf, mix_pjet_f_ddr_d_hf,
+        pjet_f_x_d_hf, mix_pjet_f_x_d_hf);
 
     /* scale by bin width */
-    scale_bin_width(pjet_f_ddr_d_ppt, mix_pjet_f_ddr_d_ppt,
-        pjet_f_ddr_d_x, mix_pjet_f_ddr_d_x, pjet_f_x, mix_pjet_f_x);
+    scale_bin_width(
+        pjet_f_ddr_d_pt, mix_pjet_f_ddr_d_pt,
+        pjet_f_ddr_d_hf, mix_pjet_f_ddr_d_hf,
+        pjet_f_x_d_pt, mix_pjet_f_x_d_pt,
+        pjet_f_x_d_hf, mix_pjet_f_x_d_hf);
 
     /* normalise to unity */
-    normalise_to_unity(pjet_f_ddr_d_x, mix_pjet_f_ddr_d_x);
 
     printf("painting..\n");
 
     auto photon_pt_selection = [&](int64_t index) {
         char buffer[128] = { '\0' };
         sprintf(buffer, "%.0f < p_{T}^{#gamma} < %.0f",
-            (*ippt)[index - 1], (*ippt)[index]);
+            (*ipt)[index - 1], (*ipt)[index]);
 
         TLatex* l = new TLatex();
         l->SetTextFont(43);
@@ -312,10 +326,10 @@ int diffaxis(char const* config, char const* output) {
         l->DrawLatexNDC(0.135, 0.75, buffer);
     };
 
-    auto x_selection = [&](int64_t index) {
+    auto hf_selection = [&](int64_t index) {
         char buffer[128] = { '\0' };
-        sprintf(buffer, "%.1f < x^{#gammaj} < %.1f",
-            (*ix)[index - 1], (*ix)[index]);
+        std::vector<int32_t> bins = { 90, 50, 30, 10, 0 };
+        sprintf(buffer, "%i - %i%%", bins[index], bins[index - 1]);
 
         TLatex* l = new TLatex();
         l->SetTextFont(43);
@@ -331,42 +345,51 @@ int diffaxis(char const* config, char const* output) {
 
     auto system = "PbPb #sqrt{s_{NN}} = 5.02 TeV"s;
 
-    auto c1 = new paper("dphi_d_ppt", hb);
+    auto c1 = new paper("dphi_d_pt", hb);
     apply_default_style(c1, system, 0., 0.8);
     c1->accessory(photon_pt_selection);
 
-    for (int64_t i = 0; i < ippt->size() - 1; ++i) {
-        c1->add((*pjet_es_f_dphi_d_ppt)[i], "escheme", "raw");
-        c1->stack((*pjet_wta_f_dphi_d_ppt)[i], "wta", "raw");
-        c1->stack((*mix_pjet_es_f_dphi_d_ppt)[i], "escheme", "mix");
-        c1->stack((*mix_pjet_wta_f_dphi_d_ppt)[i], "wta", "mix");
+    for (int64_t i = 0; i < ipt->size() - 1; ++i) {
+        c1->add((*pjet_es_f_dphi_d_pt)[i], "escheme", "raw");
+        c1->stack((*pjet_wta_f_dphi_d_pt)[i], "wta", "raw");
+        c1->stack((*mix_pjet_es_f_dphi_d_pt)[i], "escheme", "mix");
+        c1->stack((*mix_pjet_wta_f_dphi_d_pt)[i], "wta", "mix");
     }
 
-    auto c2 = new paper("ddr_d_ppt", hb);
+    auto c2 = new paper("ddr_d_pt", hb);
     apply_default_style(c2, system, 0., 12.);
     c2->accessory(photon_pt_selection);
 
-    for (int64_t i = 0; i < ippt->size() - 1; ++i) {
-        c2->add((*pjet_f_ddr_d_ppt)[i], "raw");
-        c2->stack((*mix_pjet_f_ddr_d_ppt)[i], "mix");
+    for (int64_t i = 0; i < ipt->size() - 1; ++i) {
+        c2->add((*pjet_f_ddr_d_pt)[i], "raw");
+        c2->stack((*mix_pjet_f_ddr_d_pt)[i], "mix");
     }
 
-    auto c3 = new paper("x_d_ppt", hb);
-    apply_default_style(c3, system, 0., 1.2);
-    c3->accessory(photon_pt_selection);
+    auto c3 = new paper("ddr_d_hf", hb);
+    apply_default_style(c3, system, 0., 12.);
+    c3->accessory(hf_selection);
 
-    for (int64_t i = 0; i < ippt->size() - 1; ++i) {
-        c3->add((*pjet_f_x)[i], "raw");
-        c3->stack((*mix_pjet_f_x)[i], "mix");
+    for (int64_t i = 1; i < ihf->size(); ++i) {
+        c3->add((*pjet_f_ddr_d_hf)[i], "raw");
+        c3->stack((*mix_pjet_f_ddr_d_hf)[i], "mix");
     }
 
-    auto c4 = new paper("ddr_d_x", hb);
-    apply_default_style(c4, system, 0., 20.);
-    c4->accessory(x_selection);
+    auto c4 = new paper("x_d_pt", hb);
+    apply_default_style(c4, system, 0., 1.5);
+    c4->accessory(photon_pt_selection);
 
-    for (int64_t i = 0; i < ix->size() - 1; ++i) {
-        c4->add((*pjet_f_ddr_d_x)[i], "raw");
-        c4->stack((*mix_pjet_f_ddr_d_x)[i], "mix");
+    for (int64_t i = 0; i < ipt->size() - 1; ++i) {
+        c4->add((*pjet_f_x_d_pt)[i], "raw");
+        c4->stack((*mix_pjet_f_x_d_pt)[i], "mix");
+    }
+
+    auto c5 = new paper("x_d_hf", hb);
+    apply_default_style(c5, system, 0., 1.5);
+    c5->accessory(hf_selection);
+
+    for (int64_t i = 1; i < ihf->size(); ++i) {
+        c5->add((*pjet_f_x_d_hf)[i], "raw");
+        c5->stack((*mix_pjet_f_x_d_hf)[i], "mix");
     }
 
     hb->set_binary("type");
@@ -376,12 +399,19 @@ int diffaxis(char const* config, char const* output) {
     c2->draw("pdf");
     c3->draw("pdf");
     c4->draw("pdf");
+    c5->draw("pdf");
 
+    pjet_es_f_dphi->save(type);
+    pjet_wta_f_dphi->save(type);
+    pjet_f_ddr->save(type);
     pjet_f_x->save(type);
-    pjet_es_f_dphi_d_ppt->save(type);
-    pjet_wta_f_dphi_d_ppt->save(type);
-    pjet_f_ddr_d_ppt->save(type);
-    pjet_f_ddr_d_x->save(type);
+
+    pjet_es_f_dphi_d_pt->save(type);
+    pjet_wta_f_dphi_d_pt->save(type);
+    pjet_f_ddr_d_pt->save(type);
+    pjet_f_ddr_d_hf->save(type);
+    pjet_f_x_d_pt->save(type);
+    pjet_f_x_d_hf->save(type);
 
     fout->Close();
 
