f3d87621c8123a5d6cff85b962619a09f3a800c2
Ran Bi <biran@mit.edu>
2019-08-08 22:20:03 -0400

    extract: implement residual correction

--- src/extract.C
+++ src/extract.C
@@ -9,6 +9,8 @@
 #include "../git/foliage/include/photons.h"
 #include "../git/foliage/include/triggers.h"
 
+#include "../git/history/include/history.h"
+
 #include "../git/tricks-and-treats/include/train.h"
 
 #include "TFile.h"
@@ -16,6 +18,8 @@
 
 #include <string>
 #include <vector>
+
+using namespace std::literals::string_literals;
 
 float weight_for(std::vector<int32_t> const& divisions,
                  std::vector<float> const& weights, float value) {
@@ -31,22 +35,31 @@ int extract(char const* config, char const* output) {
     auto conf = new configurer(config);
 
     auto files = conf->get<std::vector<std::string>>("files");
-    auto heavyion = conf->get<bool>("heavyion");
     auto max_entries = conf->get<int64_t>("max_entries");
+    auto array_size = conf->get<int64_t>("array_size");
+    auto tag = conf->get<std::string>("tag");
+
+    auto heavyion = conf->get<bool>("heavyion");
     auto mc_branches = conf->get<bool>("mc_branches");
     auto hlt_branches = conf->get<bool>("hlt_branches");
+    auto apply_residual = conf->get<bool>("apply_residual");
+
     auto selections = conf->get<std::vector<std::string>>("selections");
     auto paths = conf->get<std::vector<std::string>>("paths");
     auto skim = conf->get<std::vector<std::string>>("skim");
     auto jet_algo = conf->get<std::string>("jet_algo");
-    auto array_size = conf->get<int64_t>("array_size");
     auto jecs = conf->get<std::vector<std::string>>("jecs");
     auto jeu = conf->get<std::string>("jeu");
+    auto dhf = conf->get<std::vector<float>>("hf_diff");
+    auto residual = conf->get<std::string>("residual");
 
     auto pthat = conf->get<std::vector<int32_t>>("pthat");
     auto pthatw = conf->get<std::vector<float>>("pthatw");
     auto vzw = conf->get<std::vector<float>>("vzw");
 
+    auto ihf = std::make_shared<interval>(dhf);
+
+    /* load forest */
     auto forest = new train(files);
     auto chain_evt = forest->attach("hiEvtAnalyzer/HiTree", true);
     auto chain_sel = forest->attach("skimanalysis/HltTree", true);
@@ -64,12 +77,14 @@ int extract(char const* config, char const* output) {
     auto tree_jet = new jets(chain_jet, mc_branches, array_size);
     auto tree_hlt = new triggers(chain_hlt, paths);
 
+    /* setup output tree */
     TTree::SetMaxTreeSize(1000000000000LL);
 
     TFile* fout = new TFile(output, "recreate");
     TTree* tout = new TTree("pj", "photon-jet");
     auto tree_pj = new pjtree(tout, mc_branches, hlt_branches);
 
+    /* weights, corrections */
     auto JEC = new JetCorrector(jecs);
     auto JEU = new JetUncertainty(jeu);
 
@@ -77,6 +92,18 @@ int extract(char const* config, char const* output) {
     if (mc_branches) { fweight->SetParameters(
         vzw[0], vzw[1], vzw[2], vzw[3], vzw[4], vzw[5]); }
 
+    TF1** fres = nullptr;
+    if (apply_residual) {
+        TFile* fh = new TFile(residual.data(), "read");
+        auto hres = new history(fh, tag + "_es_dhf_f_pt");
+
+        fres = new TF1*[hres->size()];
+        hres->apply([&](TH1* h, int64_t index) {
+            fres[index] = h->GetFunction(
+                ("f_es_dhf_f_pt_"s + std::to_string(index)).data()); });
+    }
+
+    /* boom! */
     int64_t nentries = forest->count();
     if (max_entries) nentries = std::min(nentries, max_entries);
     for (int64_t i = 0; i < nentries; ++i) {
@@ -126,18 +153,21 @@ int extract(char const* config, char const* output) {
                 * weight_for(pthat, pthatw, tree_pj->pthat)
             : 1.f;
 
-        /* apply l2 jet energy corrections and evaluate uncertainties */
+        auto hf_x = ihf->index_for(tree_pj->hiHF);
+
+        /* apply jet energy corrections and evaluate uncertainties */
         for (int64_t j = 0; j < tree_pj->nref; ++j) {
             JEC->SetJetPT((*tree_pj->rawpt)[j]);
             JEC->SetJetEta((*tree_pj->jteta)[j]);
             JEC->SetJetPhi((*tree_pj->jtphi)[j]);
 
             float corr = JEC->GetCorrectedPT();
-            (*tree_pj->jtpt)[j] = corr;
+            float cres = (apply_residual) ? fres[hf_x]->Eval(corr) : 1.f;
+            (*tree_pj->jtpt)[j] = corr / cres;
 
             auto unc = JEU->GetUncertainty();
-            tree_pj->jtpt_up->push_back(corr * (1. - unc.first));
-            tree_pj->jtpt_down->push_back(corr * (1. + unc.second));
+            tree_pj->jtpt_up->push_back(corr / cres * (1. - unc.first));
+            tree_pj->jtpt_down->push_back(corr / cres * (1. + unc.second));
         }
 
         tout->Fill();
