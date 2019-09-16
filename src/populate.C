08d19b256567eb19e67907351147e39c6200417e
Ran Bi <biran@mit.edu>
2019-09-16 19:09:12 -0400

    populate: fill with weights
    important for simulated samples.

--- src/populate.C
+++ src/populate.C
@@ -56,7 +56,7 @@ void fill_axes(pjtree* pjt, int64_t pthf_x, multival* mr,
                memory<TH1F>* pjet_f_ddr,
                memory<TH1F>* pjet_f_jpt,
                memory<TH1F>* pjet_f_r) {
-    (*nevt)[pthf_x]->Fill(1.);
+    (*nevt)[pthf_x]->Fill(1., pjt->weight);
 
     for (int64_t j = 0; j < pjt->nref; ++j) {
         auto jet_pt = (*pjt->jtpt)[j];
@@ -79,24 +79,24 @@ void fill_axes(pjtree* pjt, int64_t pthf_x, multival* mr,
         auto photon_jet_dphi = std::abs(photon_phi - jet_phi);
         auto photon_wta_dphi = std::abs(photon_phi - jet_wta_phi);
 
-        (*pjet_es_f_dphi)[pthf_x]->Fill(photon_jet_dphi);
-        (*pjet_wta_f_dphi)[pthf_x]->Fill(photon_wta_dphi);
+        (*pjet_es_f_dphi)[pthf_x]->Fill(photon_jet_dphi, pjt->weight);
+        (*pjet_wta_f_dphi)[pthf_x]->Fill(photon_wta_dphi, pjt->weight);
 
         /* require back-to-back jets */
         if (photon_jet_dphi < 0.875_pi) { continue; }
 
-        (*pjet_f_jpt)[pthf_x]->Fill(jet_pt);
-        (*pjet_f_x)[pthf_x]->Fill(jet_pt / photon_pt);
+        (*pjet_f_jpt)[pthf_x]->Fill(jet_pt, pjt->weight);
+        (*pjet_f_x)[pthf_x]->Fill(jet_pt / photon_pt, pjt->weight);
 
         double jt_deta = jet_eta - jet_wta_eta;
         double jt_dphi = revert_radian(jet_phi - jet_wta_phi);
         double jt_dr = std::sqrt(jt_deta * jt_deta + jt_dphi * jt_dphi);
 
-        (*pjet_f_ddr)[pthf_x]->Fill(jt_dr);
+        (*pjet_f_ddr)[pthf_x]->Fill(jt_dr, pjt->weight);
 
         if (jet_pt >= 200) { continue; }
 
-        (*pjet_f_r)[pthf_x]->Fill(mr->index_for(v{jt_dr, jet_pt}));
+        (*pjet_f_r)[pthf_x]->Fill(mr->index_for(v{jt_dr, jet_pt}), pjt->weight);
     }
 }
 
